911c709623667b4fd339fa0b893da62d3fe9370b
Angelo <philipangeloang1@gmail.com>
2024-07-04 21:26:16 +0800

    Initial Landing Page Done

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,22 +1,44 @@
 import Link from "next/link";
+import { MdOutlineCalendarToday } from "react-icons/md";
+import { LuUsers2 } from "react-icons/lu";
+import { VscGraphLine } from "react-icons/vsc";
+import { PiGlobeLight } from "react-icons/pi";
 
-import { getServerAuthSession } from "~/server/auth";
 import { Button } from "./_components/ui/button";
 
 // const session = await getServerAuthSession();
 // #9FE96E #A4A7F6 #636363 #000000 #FFFFFF
 
 export default async function Home() {
-  const session = await getServerAuthSession();
-
   return (
     <main className="w-full p-4">
-      <div className="mx-auto mt-24 flex max-w-screen-2xl flex-col items-center justify-center">
+      <div className="relative mx-auto mt-16 flex max-w-screen-xl flex-col items-center justify-center">
         <div className="rounded-full bg-black/5 px-2 py-1 text-sm">
           v0.1 - Initial Application Launch
         </div>
-        <div className="mt-8 text-center text-[3.25rem] leading-[4.5rem]">
-          Manage your <br /> finances with ease
+        <div className="relative mt-8 text-center text-[3.25rem] leading-[4.5rem]">
+          <PiGlobeLight
+            size={35}
+            className="absolute left-[-5rem] top-[-2rem] hidden rotate-[-12deg] rounded-md bg-black/10 p-1 md:block"
+          />
+          <LuUsers2
+            size={35}
+            className="absolute bottom-[-8rem] left-[-8rem] hidden rotate-[-12deg] rounded-md bg-black/10 p-1 md:block"
+          />
+          <MdOutlineCalendarToday
+            size={35}
+            className="absolute right-[-7rem] top-[1rem] hidden rotate-[12deg] rounded-md bg-black/10 p-1 md:block"
+          />
+          <VscGraphLine
+            size={35}
+            className="absolute bottom-[-11rem] right-[-3rem] hidden rotate-[12deg] rounded-md bg-black/10 p-1 md:block"
+          />
+          <div>
+            Manage your <br /> finances with{" "}
+            <span className="rounded-full shadow-md shadow-main-green">
+              ease
+            </span>
+          </div>
         </div>
         <div className="mt-3 text-center">
           Revolutionize finance management. Track everything religiously. <br />{" "}
@@ -28,6 +50,8 @@ export default async function Home() {
         >
           <Link href="api/auth/signin">Start for free</Link>
         </Button>
+
+        <img src="/home-temp-image.jpg" className="w-full bg-cover pt-20" />
       </div>
     </main>
   );
