df0c97d4f80a7364e183f8dfdb0ba1e9919c0944
Angelo <philipangeloang1@gmail.com>
2024-07-09 01:33:01 +0800

    Ready for tomorrow work

--- src/app/api/auth/verify-request/page.tsx
+++ src/app/api/auth/verify-request/page.tsx
@@ -1,14 +1,17 @@
 import Link from "next/link";
 import { MdOutlineEmail } from "react-icons/md";
+import { Button } from "~/app/_components/ui/button";
 
 const VerifyRequest = async () => {
   return (
     <div className="flex h-screen w-screen flex-col items-center justify-center">
-      <MdOutlineEmail size={100} color="#9FE96E" />
-      <p className="text-4xl font-bold  ">Check your email</p>
+      <MdOutlineEmail size={80} color="#9FE96E" />
+      <p className="my-2 text-5xl font-bold ">Check your email</p>
       <p>A sign in link has been sent to your email address</p>
       <p>Click the link on your email to sign in</p>
-      <Link href="/">Go back to home page</Link>
+      <Button asChild className="my-5 rounded-full bg-black">
+        <Link href="/">Go back to home page</Link>
+      </Button>
     </div>
   );
 };
